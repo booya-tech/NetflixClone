98b99ac1b249cf187e8daf3a009c2240165dd6e3
Boo Pannachai <booya.pannachai@gmail.com>
2025-06-24 09:12:47 +0700

    netflix clone: updated part 3, implemented TableHeader

---
+++ NetflixClone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,46 @@
+//
+//  HeroHeaderUIView.swift
+//  NetflixClone
+//
+//  Created by Panachai Sulsaksakul on 6/23/25.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(heroImageView)
+        addGradient()
+    }
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
